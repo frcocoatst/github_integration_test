434bea04dbe11d38afd2369e31906af5e1f6c8bd
fr <fr@fr.com>
2022-01-07 10:22:22 +0100

    Initial Commit

---
+++ github_integration_test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  github_integration_test
+//
+//  Created by Friedrich HAEUPL on 07.01.22.
+//  Copyright © 2022 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ github_integration_test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  github_integration_test
+//
+//  Created by Friedrich HAEUPL on 07.01.22.
+//  Copyright © 2022 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
